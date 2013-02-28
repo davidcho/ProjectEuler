ef901dd663b30bf9a6c21e481698bd7d628c6f11
David Cho <dc664@cornell.edu>
2013-02-27 23:36:05 -0500

    Traverse works with BigIntegers now

--- util/Grid.java
+++ util/Grid.java
@@ -1,5 +1,7 @@
 package util;
 
+import java.math.BigInteger;
+
 public class Grid {
 	/**
 	 * Computes the number of possible ways of going from top left corner to
@@ -8,11 +10,11 @@ public class Grid {
 	 * @param n
 	 *            Represents number of rows and columns of the grid
 	 */
-	public static int traverse(int n) {
+	public static BigInteger traverse(int n) {
 		return traverse(0, 0, n, n);
 	}
 
-	public static int traverse(int xMax, int yMax) {
+	public static BigInteger traverse(int xMax, int yMax) {
 		return traverse(0, 0, xMax, yMax);
 	}
 
@@ -28,26 +30,26 @@ public class Grid {
 	 *            Max y coor
 	 * @return
 	 */
-	public static int traverse(int x, int y, int xMax, int yMax) {
+	public static BigInteger traverse(int x, int y, int xMax, int yMax) {
 		// if x is at rightmost, then we have 2 cases:
 		// (1) if y is at bottommost, then we are done
 		// (2) else we can only move down to reach endpoint
 		// if y is at bottommost, we can only move right to reach endpoint
 
 		if (x == xMax)
-			return y == yMax ? 0 : 1;
+			return y == yMax ? BigInteger.ZERO : BigInteger.ONE;
 		else if (y == yMax)
-			return 1;
+			return BigInteger.ONE;
 		else if (x == y)
-			return 2 * traverse(x + 1, y, xMax, yMax);
+			return traverse(x + 1, y, xMax, yMax).multiply(new BigInteger("2"));
 		else
-			return traverse(x + 1, y, xMax, yMax)
-					+ traverse(x, y + 1, xMax, yMax);
+			return traverse(x + 1, y, xMax, yMax).add(
+					traverse(x, y + 1, xMax, yMax));
 	}
 
 	public static void main(String[] args) {
 		// assert (traverse(2) == 6);
-		for (int i = 20; i <= 20; i++) {
+		for (int i = 18; i <= 18; i++) {
 			long start = System.currentTimeMillis();
 			System.out.println(i + ": " + traverse(i));
 			System.out.println(System.currentTimeMillis() - start);
